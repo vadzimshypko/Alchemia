f7649a20552df22954fefa19e8decd94f61366b6
vadim <vadim@HP>
2021-11-11 14:01:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alchemia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
