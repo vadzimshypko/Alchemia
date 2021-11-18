74cebdf9213356cfd9a90fe67f5114f600561006
ihnashchenka <ignashchanka@gmail.com>
2021-11-18 12:43:25 +0300

    add data validation module

---
+++ data_validation/DataValidator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DataValidation
+{
+    class DataValidator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LoadJsonElements();
+            LoadJsonCombinations();
+          
+        }
+
+        public static void LoadJsonElements()
+        {
+            using (StreamReader r = new StreamReader("input/elements.json")) 
+            {
+                string json = r.ReadToEnd();
+                List<InputElement> elements = JsonConvert.DeserializeObject<List<InputElement>>(json);
+
+                foreach (object el in elements)
+                {
+                    Console.WriteLine(el.ToString());
+                }
+            }
+        }
+
+        public static void LoadJsonCombinations()
+        {
+            using (StreamReader r = new StreamReader("input/combinations.json"))
+            {
+                string json = r.ReadToEnd();
+                List<InputCombination> combinations = JsonConvert.DeserializeObject<List<InputCombination>>(json);
+
+                foreach (object  comb in combinations)
+                {
+                    Console.WriteLine(comb.ToString());
+                }
+            }
+        }
+
+    }
+
+    public class InputCombination
+    {
+        public string input1;
+        public string input2;
+        public List<string> output;
+
+        public override string ToString()
+        {
+            string outputSring = "";
+            foreach (object element in output)
+            {
+                outputSring += element + ",";
+            }
+            return "input1:" + input1 + ";input2:" + input2 + ";output:" + outputSring;
+        }
+
+
+    }
+}
---
+++ data_validation/InputElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataValidation
+{
+    public class InputElement
+    {
+        public string id_name;
+        public string display_name;
+        public string image;
+
+        public override string ToString()
+        {
+            return "id_name:" + id_name + ";dispaly_name:" + display_name + ";image:" + image;
+        }
+
+    }
+}
